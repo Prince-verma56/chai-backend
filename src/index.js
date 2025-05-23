1db05989266af6f3828aaab3de34b1da505c5e26
Prince Verma <pvcodingz96@gmail.com>
2025-05-23 08:36:09 +0530

    connect to mongoDB

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import dotenv from "dotenv";
+dotenv.config({
+    path: "./.env"
+});
+
+import connectDB from "./db/index.js";
+
+
+
+
+
+
+connectDB();
+
+
+
+// ______First Approach__________
+
+
+
+// import express from "express";
+// import dotenv from "dotenv";
+
+// const app = express();
+
+// ; (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.error("Error connecting to MongoDB:", error);
+//         });
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`Server is running on port https://localhost:${process.env.PORT}`);
+//         })
+//     }
+//     catch (error) {
+//         console.error("Error connecting to MongoDB:", error);
+//         throw error;
+//     }
+// })()
